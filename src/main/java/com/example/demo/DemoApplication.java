2b0b869e6167e9715b2b3ebecc226186511bbe06
BhagyaJan <59689010+BhagyaJan@users.noreply.github.com>
2023-03-23 22:29:42 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("commit from eclipse Nancy Steve");
+		System.out.println("commit from eclipse Nancy Steve from github");
 	}
 
 }
